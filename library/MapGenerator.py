199a7225dbf072a0beab0b2a5264b00b3d0dd5ed
g.largange <g.largange@technologyandstrategy.com>
2020-12-04 16:59:09 +0100

    Started work on Pathfinding algorithm.

--- library/MapGenerator.py
+++ library/MapGenerator.py
@@ -11,7 +11,7 @@
 
 import random
 
-from PyQt5.QtCore import QPoint
+from PyQt5.QtCore import QPoint, QPointF
 from PyQt5.QtGui import QPolygonF
 from library import MathsFormulas
 
@@ -43,12 +43,10 @@ class MapGenerator():
 
         self.polygonsList = []
 
-        c = 0
         for i in range(int((self.mapW / self.mapS))):
             self.gameMap.append([])
             for j in range(int((self.mapH / self. mapS))):
-                self.gameMap[c].append(0)
-            c += 1
+                self.gameMap[i].append(0)
 
         # print("######### MAP INITIALISATION ########")
         # for element in self.gameMap:
@@ -104,7 +102,7 @@ class MapGenerator():
                     # print("An obstacle is already present in the desired area.")
                     return None
 
-        # print("The obstacle:", x, y, w, h, "can be placed")
+        # print("The obstacle:", y, x, h, w, "can be placed")
         return [x, y, w, h]
 
     def randomObstacle(self):
@@ -138,14 +136,14 @@ class MapGenerator():
         h = ObsAsList[3]
         poly = QPolygonF()
 
-        for i in range(x, x + w + 1):
-            for j in range(y, y + h + 1):
+        for i in range(x, x + w):
+            for j in range(y, y + h):
                 self.gameMap[i][j] = 1
 
         polyTL = QPoint(y * 1000, x * 1000)
-        polyBL = QPoint(y * 1000, (x + w + 1) * 1000)
-        polyBR = QPoint((y + h + 1) * 1000, (x + w + 1) * 1000)
-        polyTR = QPoint((y + h + 1) * 1000, x * 1000)
+        polyBL = QPoint(y * 1000, (x + w) * 1000)
+        polyBR = QPoint((y + h) * 1000, (x + w) * 1000)
+        polyTR = QPoint((y + h) * 1000, x * 1000)
 
         poly<<polyTL<<polyBL<<polyBR<<polyTR
 
@@ -199,3 +197,81 @@ class MapGenerator():
         print("###############################")
 
         return self.polygonsList
+
+
+class Node():
+
+    iGrid = 0
+    jGrid = 0
+    traversible = True
+    gCost = 0
+    hCost = 0
+
+    def __init__(self, iGrid, jGrid, traversible):
+        self.iGrid = iGrid
+        self.jGrid = jGrid
+        self.traversible = traversible
+
+    def setGcost(self, gCostValue):
+        self.gCost = gCostValue
+
+    def setHcost(self, hCostValue):
+        self.hCost = hCostValue
+
+    def fCost(self):
+        return self.gCost + self.hCost
+
+    def retScenePos(self):
+        return QPointF(self.jGrid * 1000, self.iGrid * 1000)
+
+
+class Astar():
+
+    def __init__(self, gameMap):
+        self.allNodes = []
+        self.openList = []
+        self.closedList = []
+        self.currentNode = None
+        self.currentNodeParent = None
+
+        for i in range(len(gameMap)):
+            self.allNodes.append([])
+            for j in range(len(gameMap[0])):
+                traversible = True if (gameMap[i][j] == 0) else False
+                self.allNodes[i].append(Node(i, j, traversible))
+
+    def getNode(self, i, j):
+        return self.allNodes[i][j]
+
+    def getNeighbours(self, node):
+        iMin = node.iGrid - 1
+        iMax = node.iGrid + 1
+        jMin = node.jGrid - 1
+        jMax = node.jGrid + 1
+        neighbours = []
+
+        if node.iGrid == 0:
+            iMin = node.iGrid
+        if node.iGrid == (len(self.allNodes) - 1):
+            iMax = node.iGrid
+        if node.jGrid == 0:
+            jMin = node.jGrid
+        if node.jGrid == (len(self.allNodes[0]) - 1):
+            jMax = node.jGrid
+
+        for i in range(iMin, iMax + 1):
+            for j in range(jMin, jMax + 1):
+                if (i == node.iGrid) & (j == node.jGrid):
+                    continue
+                else:
+                    neighbours.append(self.allNodes[i][j])
+
+        return neighbours
+
+    def findPath(self, startPos, targetPos):
+        startNode = self.getNode(int(startPos.y() / 1000),
+                                 int(startPos.x() / 1000))
+        targetNode = self.getNode(int(targetPos.y() / 1000),
+                                  int(targetPos.x() / 1000))
+
+        self.openList.append(startNode)
