5a053e9d9c68d0a0adddf9dc770c462802a183c6
g.largange <g.largange@technologyandstrategy.com>
2020-12-10 17:30:32 +0100

    Started added docstrings and in-line comment to each file.

--- library/InGameData.py
+++ library/InGameData.py
@@ -5,12 +5,37 @@
     Author: Grégory LARGANGE
     Date created: 19/10/2020
     Last modified by: Grégory LARGANGE
-    Date last modified: 19/10/2020
+    Date last modified: 10/12/2020
     Python version: 3.8.1
 '''
 
 
 class ProjectileData():
+    """
+
+    A class holding projectiles parameters
+
+    ...
+
+    Attributes
+    ----------
+    speeds_shellType : list of int
+        A list of speeds per shell type.
+
+    ranges_shellSize : list of int
+        A list of ranges per shell size.
+
+    damage_type : list of lists of int
+        A list per shell size of sublist of damage per shell type.
+
+    pen_values : list of sublists of int
+        A list per shell size of sublist of penetration per shell type.
+
+    Methods
+    -------
+    None
+
+    """
 
     speeds_shellType = [150, 125]
     ranges_shellSize= [9000, 15000, 21000]
@@ -19,6 +44,50 @@ class ProjectileData():
 
 
 class RadioCommunications():
+    """
+
+    A class implementing a form of 'radio communication' between ships of the
+    same fleet.
+
+    ...
+
+    Attributes
+    ----------
+    alliedShips : list of Ships
+        A list of all allied ships on the scene.
+
+    ennemyShips : list of Ships
+        A list of all ennemy ships on the scene.
+
+    alliedDetectedShips : list of Ships
+        A list of all ennemy ships detected by all allied ships.
+
+    ennemyDetectedShips : list of Ships
+        A list of all allied ships detected by all ennemy ships.
+
+    radioCommsRate : int
+        The period between two radio emmissions.
+
+    Methods
+    -------
+    __init__(mainClock : MainClock, gameScene : GameScene)
+        The constructor of the class.
+
+    updateShipLists()
+        Updates the allied ship list and the ennemy ship list.
+
+    fixedUpdate()
+        Performs a radio communication each radioCommsRate.
+
+    gatherDetectedShips()
+        Gather all ennemy ships detected by allied ships and all ennemy ships 
+        detected by allied ships.
+
+    transmitDetectedShips()
+        Transmit to each allied ship a list of all detected ennemy ships.
+        Transmit to each ennemy ship a list of all detected allied ships.
+
+    """
 
     alliedShips = []
     ennemyShips = []
@@ -27,6 +96,24 @@ class RadioCommunications():
     radioCommsRate = 19
 
     def __init__(self, mainClock, gameScene):
+        """
+
+        Parameters
+        ----------
+        mainClock : MainClock
+            The main clock of the game.
+        gameScene : GameScene
+            the main display for the game.
+
+        Returns
+        -------
+        None.
+
+        Summary
+        -------
+        The constructor of the class.
+
+        """
         self.gameScene = gameScene
         self.clock = mainClock
         self.next_radioComm = self.radioCommsRate
@@ -34,6 +121,17 @@ class RadioCommunications():
         self.clock.clockSignal.connect(self.fixedUpdate)
 
     def updateShipLists(self):
+        """
+
+        Returns
+        -------
+        None.
+
+        Summary
+        -------
+        Updates the alliedShips list and the ennmyShips list.
+
+        """
         for ship in self.gameScene.shipList.values():
             if ship.shipTag == "ALLY":
                 self.alliedShips.append(ship)
@@ -41,6 +139,18 @@ class RadioCommunications():
                 self.ennemyShips.append(ship)
 
     def fixedUpdate(self):
+        """
+
+        Returns
+        -------
+        None.
+
+        Summary
+        -------
+        Synchronisation with the game main clock. Each radioCommsRate, performs
+        the 'radio comm' process.
+        
+        """
         if self.next_radioComm <= 0:
             self.gatherDetectedShips()
             self.transmitDetectedShips()
@@ -49,6 +159,18 @@ class RadioCommunications():
             self.next_radioComm -= 1
 
     def gatherDetectedShips(self):
+        """
+
+        Returns
+        -------
+        None.
+
+        Summary
+        -------
+        Gather all detected ships from all ships, for allies and ennemies
+        respectively.
+
+        """
         tmpADS = []
         tmpEDS = []
 
@@ -66,6 +188,18 @@ class RadioCommunications():
         self.ennemyDetectedShips = tmpEDS
 
     def transmitDetectedShips(self):
+        """
+
+        Returns
+        -------
+        None.
+
+        Summary
+        -------
+        Transmit to all ships the list of all detected ships, for allies and
+        ennemies respectively.
+
+        """
         for alliedShip in self.alliedShips:
             alliedShip.receiveRadioComm(self.alliedDetectedShips)
         for ennemyShip in self.ennemyShips:
--- library/MathsFormulas.py
+++ library/MathsFormulas.py
@@ -5,7 +5,7 @@
     Author: Grégory LARGANGE
     Date created: 16/10/2020
     Last modified by: Grégory LARGANGE
-    Date last modified: 25/11/2020
+    Date last modified: 10/12/2020
     Python version: 3.8.1
 '''
 
@@ -13,45 +13,222 @@ import math
 
 from PyQt5.QtCore import QPointF
 
+
 class Geometrics():
+    """
+
+    A class implementing uselful euclidian geometrics functions.
+
+    Attributes
+    ----------
+    None.
+
+    Methods
+    -------
+    parallelepiped_Center(p_topLeft : QPointF, p_width : float, p_height : float)
+        Returns the center of a parallelepiped.
+
+    distance_A_B(pointA : QPointF, pointB : QPointF)
+        Returns the distance betwenn pointA and pointB.
+
+    pythagore(sideB : float, sideC : float)
+        To be applied in a sqaure rectangle. Returns the hypothenuse of a triangle
+        of sides A, B, C, where A is the hypothenuse.
+
+    alKashi_side(sideB : float, sideC : float, angleInRad : float)
+        Returns side A of a regular triangle of sides A, B, C and angle a opposite
+        to side A.
+
+    alKashi_angle_cos(sideA : float, sideB : float, sideC : float)
+        Returns angle a (in radians) opposite to side A in any regular triangle
+        of sides A, B, C.
+
+    smallestAngle(targetAngleDeg : float, curAngleDeg : float)
+        Returns the smallest angle possible to go from curAngleDeg to
+        targetAngleDeg.
+
+    checkSegmentsIntersect(segmentAB : list of QPointF, segmentCD : list of QPointF)
+        Returns True if there is an intersection between segmentAB and segmentCD,
+        False otherwise.
+
+    """
 
     def parallelepiped_Center(self, p_topLeft, p_width, p_height):
+        """
+
+        Parameters
+        ----------
+        p_topLeft : QPointF
+            The top left point of the parallelepiped.
+        p_width : float
+            The width of the parallelepiped.
+        p_height : float
+            The height of the parallelepiped.
+
+        Returns
+        -------
+        p_center : QPointF
+            The center of the parallelepiped.
+
+        Summary
+        -------
+        Returns the center of the parallelepiped.
+
+        """
         p_center = QPointF(p_topLeft.x() + p_width / 2,
                            p_topLeft.y() + p_height / 2)
         return p_center
 
     def distance_A_B(self, pointA, pointB):
+        """
+
+        Parameters
+        ----------
+        pointA : QPointF
+            A point of the scene.
+        pointB : QPointF
+            A point of the scene.
+
+        Returns
+        -------
+        distance : Float
+            The distance between pointA and pointB.
+
+        Summary
+        -------
+        Returns the distance between pointA and pointB.
+
+        """
         distance = math.sqrt(
             pow(pointB.x() - pointA.x(), 2) +\
             pow(pointB.y() - pointA.y(), 2))
         return distance
 
     def pythagore(self, sideB, sideC):
+        """
+
+        Parameters
+        ----------
+        sideB : float
+            The length of a side of the square angle of the triangle.
+        sideC : float
+            The length of an other side of the square angle of the triangle.
+
+        Returns
+        -------
+        sideA : float
+            The length of sideA, the hypothenuse of the triangle.
+
+        Summary
+        -------
+        VALID FOR SQUARE TRIANGLES ONLY.
+        returns the length of the hypothenuse of the triangle.
+
+        """
         sideA = math.sqrt(pow(sideB, 2) + pow(sideC, 2))
         return sideA
 
     def alKashi_side(self, sideB, sideC, angleInRad):
+        """
+
+        Parameters
+        ----------
+        sideB : float
+            The length of a side of the a angle of the triangle.
+        sideC : float
+            The length of an other side of the a angle of the triangle.
+        angleInRad : float
+            The reference angle used for calculations.
+
+        Returns
+        -------
+        sideA : float
+            The length of sideA of the triangle.
+
+        Summary
+        -------
+        Returns the length of the sideA of the triangle.
+
+        """
         sideA = math.sqrt(sideB ** 2 + sideC ** 2 - 2 * sideB * sideC * math.cos(angleInRad))
         return sideA
 
     def alKashi_angle_cos(self, sideA, sideB, sideC):
+        """
+
+        Parameters
+        ----------
+        sideA : float
+            The length of sideA of the triangle.
+        sideB : float
+            The length of sideB of the triangle.
+        sideC : float
+            The length of sideC of the triangle.
+
+        Returns
+        -------
+        angleInRad : float
+            The value in radians of the anfle a opposite to sideA of the triangle.
+
+        Summary
+        -------
+        Returns the angle a in radians.
+
+        """
         angleInRad = math.acos(-(sideA ** 2 - (sideB ** 2 + sideC ** 2)) / (2 * sideB * sideC))
         return angleInRad
 
-    def smallestAngle(self, targetAngleDegrees, currentAngleDegrees):
-        offset = 360 if targetAngleDegrees < currentAngleDegrees else -360
-        diff1 = targetAngleDegrees - currentAngleDegrees
-        diff2 = targetAngleDegrees - currentAngleDegrees + offset
+    def smallestAngle(self, targetAngleDeg, curAngleDeg):
+        """
+
+        Parameters
+        ----------
+        targetAngleDeg : float
+            The target angle.
+        curAngleDeg : float
+            The initial angle.
+
+        Returns
+        -------
+        diff : float
+            The smallest angle.
+
+        Summary
+        -------
+        Returns the smallest angle between curAngleDeg and targetAngleDeg.
+
+        """
+        offset = 360 if targetAngleDeg < curAngleDeg else -360
+        diff1 = targetAngleDeg - curAngleDeg
+        diff2 = targetAngleDeg - curAngleDeg + offset
         diff = diff1 if abs(diff1) < abs(diff2) else diff2
         return diff
 
     def checkSegmentsIntersect(self, segmentAB, segmentCD):
+        """
+
+        Parameters
+        ----------
+        segmentAB : list of QPointF
+            A segment of the scene.
+        segmentCD : list of QPointF
+            A segment of the scene.
+
+        Returns
+        -------
+        bool
+            True if intersection, False otherwise.
+
+        Summary
+        -------
+        Returns True if segmentAB and segmentCD have a real intersection, False
+        otherwise.
+
+        """
         XA = segmentAB[0].x()
         YA = segmentAB[0].y()
         XB = segmentAB[1].x()
         YB = segmentAB[1].y()
-        print("SEGMENT AB:","A" + "(" + str(XA) + ", " + str(YA) + ")",
-              "B" + "(" + str(XB) + ", " + str(YB) + ")")
 
         XC = segmentCD[0].x()
         YC = segmentCD[0].y()
@@ -61,18 +238,21 @@ class Geometrics():
         IintX = [max(min(XA, XB), min(XC, XD)),
                  min(max(XA, XB), max(XC, XD))]
 
-        if max(XA, XB) < min(XC, XD):
-            print("No common intervals on X. No Intersection possible")
+        if (max(XA, XB) < min(XC, XD)) | (min(XA, XB) > max(XC, XD)):
             return False
 
         if ((XA - XB) == 0) | ((XC - XD) == 0):
             if (XA - XB) == 0:
-                if max(YC, YD) < min(YA, YB):
+                if (XA < min(XC, XD)) | (XA > max(XC, XD)):
+                    return False
+                elif (min(YA, YB) > max(YC, YD)) | (max(YA, YB) < min(YC, YD)):
                     return False
                 else:
                     return True
             else:
-                if max(YA, YB) < min(YC, YD):
+                if (XC < min(XA, XB)) | (XC > max(XA, XB)):
+                    return False
+                elif (min(YC, YD) > max(YA, YB)) | (max(YC, YD) < min(YA, YB)):
                     return False
                 else:
                     return True
@@ -99,23 +279,138 @@ class Geometrics():
 
 
 class Cinematics():
+    """
+
+    A class implementing uselful basic cinematic functions.
+
+    ...
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    movementBy(originPoint : QPointF, moveDistance : float, angleInRad : float)
+        Calculates the new position of a point after a movement.
+
+    brakeDistance(vinit : float, deceleration : float)
+        Calculates the breaking distance of an object. Assumes a uniform linear
+        movement.
+
+    rotationRadius(speed : float, rotSpeedInD_s : float)
+        Calculates the rotation radius of an object with a circular uniform
+        movement.
+
+    rotationCenters(centerPos : QPointF, heading : float, speed : float,
+                    rotSpeedInD_s : float)
+        Calculates the position of rotation centers of an object with a circular
+        uniform movement.
+
+    """
 
     def movementBy(self, originPoint, moveDistance, angleInRad):
+        """
+
+        Parameters
+        ----------
+        originPoint : QPOintF
+            The initial position of the moving point.
+        moveDistance : float
+            The absolute ditance travelled by the point.
+        angleInRad : float
+            The angle of travel of the moving point.
+
+        Returns
+        -------
+        newPoint : QPointF
+            The position of the point after its movement.
+
+        Summary
+        -------
+        Returns the new position of a point after a movement of moveDistance in
+        the angleInRad direction.
+
+        """
         newX = round(originPoint.x() + moveDistance * math.cos(angleInRad), 2)
         newY = round(originPoint.y() + moveDistance * math.sin(angleInRad), 2)
         newPoint = QPointF(newX, newY)
         return newPoint
 
     def brakeDistance(self, vinit, deceleration):
+        """
+
+        Parameters
+        ----------
+        vinit : float
+            The initial velocity of the object.
+        deceleration : float
+            The constant deceleration of an object.
+
+        Returns
+        -------
+        brakeD : float
+            The breaking distance of the object.
+
+        Summary
+        -------
+        Returns the breaking distance of an object breaking from the initial
+        speed vinit with a deceleration deceleration.
+
+        """
         brakeD = int((deceleration / 2) * (-vinit / deceleration) ** 2 - ((vinit ** 2) / deceleration))
         return brakeD
 
-    def rotationRadius(self, speed, rotationSpeedInD_s):
-        radius = int((180 * speed) / (math.pi * rotationSpeedInD_s))
+    def rotationRadius(self, speed, rotSpeedInD_s):
+        """
+
+        Parameters
+        ----------
+        speed : float
+            The linear speed of the object.
+        rotSpeedInD_s : float
+            The rotation speed of the object.
+
+        Returns
+        -------
+        radius : float
+            The radius of the object's turn.
+
+        Summary
+        -------
+        Returns the turn radius of an object with a linear speed speed and a
+        rotation speed rotSpeedInD_s.
+
+        """
+        radius = int((180 * speed) / (math.pi * rotSpeedInD_s))
         return radius
 
-    def rotationCenters(self, centerPos, heading, speed, rotationSpeedInD_s):
-        radius = self.rotationRadius(speed, rotationSpeedInD_s)
+    def rotationCenters(self, centerPos, heading, speed, rotSpeedInD_s):
+        """
+
+        Parameters
+        ----------
+        centerPos : QPointF
+            The center of the object.
+        heading : float
+            The movement's direction of the object.
+        speed : float
+            The linear speed of the object.
+        rotSpeedInD_s : float
+            The rotation speed of the object.
+
+        Returns
+        -------
+        list
+            List of the rotation centers.
+
+        Summary
+        -------
+        Calculates the position of the rotation centers on either side of the
+        object, and returns a list with both of them.
+
+        """
+        radius = self.rotationRadius(speed, rotSpeedInD_s)
 
         h_l = math.radians(heading - 90)
         h_r = math.radians(heading + 90)
@@ -131,8 +426,49 @@ class Cinematics():
 
 
 class Controllers():
+    """
+
+    A class implementing controllers.
+
+    ...
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    proportional(targetValue : float, currentValue : float, maxGain : float,
+                 customDiff : optional float)
+        Applies a proportional controller to the input.
+
+    """
 
     def proportional(self, targetValue, currentValue, maxGain, customDiff=None):
+        """
+
+        Parameters
+        ----------
+        targetValue : float
+            The desired value.
+        currentValue : float
+            The current value.
+        maxGain : float
+            The maximum gain that can be applied to the input.
+        customDiff : float, optional
+            User can input here its custom error calculation. The default is None.
+
+        Returns
+        -------
+        gain : float
+            The calculated gain to apply.
+
+        Summary
+        -------
+        Calculates the gain to apply to currentValue in order to reach target
+        value while not exceeing maxGain.
+
+        """
         staticGain = 1
 
         if customDiff is None:
