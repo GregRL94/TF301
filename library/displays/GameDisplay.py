6d886d3ba8d5054a52ab6acb6e98aac4d9ae8d8a
g.largange <g.largange@technologyandstrategy.com>
2021-10-22 11:04:22 +0200

    Minor change

--- library/displays/GameDisplay.py
+++ library/displays/GameDisplay.py
@@ -5,7 +5,7 @@
     Author: Grégory LARGANGE
     Date created: 12/10/2020
     Last modified by: Grégory LARGANGE
-    Date last modified: 18/10/2021
+    Date last modified: 22/10/2021
     Python version: 3.8.1
 """
 
@@ -65,7 +65,7 @@ class GameScene(QGraphicsScene):
                     if isinstance(selected_item, Ship):
                         for item in self.selectedItems():
                             if selected_item.data(1) == "ISLAND":
-                                print("MOVEMENT NOT POSSIBLE")
+                                continue
                             elif selected_item.data(1) != item.data(1):
                                 item.setTarget(selected_item)
                             elif selected_item.data(1) == item.data(1):
